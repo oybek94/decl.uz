dcd69657d96b0d2abfd60716ce9545001dd990a4
oybek94 <oybek94@bk.ru>
2022-05-01 22:20:19 +0500

    add FAQ page

--- js/main.js
+++ js/main.js
@@ -12,3 +12,23 @@ burgerMenu.addEventListener("click", () => {
 		navbarMenu.removeAttribute("style");
 	}
 });
+
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
